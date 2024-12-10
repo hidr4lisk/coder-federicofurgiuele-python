92e3a69ddabb16a6db50632ad99e40303c5b084f
hidr4lisk <federico.furgiuele@gmail.com>
2024-12-10 10:48:45 -0300

    Ya podemos crear usuarios

--- src/biblioteca/admin.py
+++ src/biblioteca/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Usuario, Libro, Prestamo
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Libro)
+admin.site.register(Prestamo)
--- src/biblioteca/views.py
+++ src/biblioteca/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Usuario
 
 def index(request):
     return render(request, "biblioteca/index.html")
+
+def about(request):
+    return render(request, "biblioteca/about.html")
+
+def crear_usuario(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        email = request.POST.get("email")
+        telefono = request.POST.get("telefono")
+        
+        # Crear el usuario
+        Usuario.objects.create(nombre=nombre, email=email, telefono=telefono)
+        return redirect('index')  
+    
+    return render(request, "biblioteca/crear_usuario.html")
